e3134f270aa85da1dcfb071e66f7771f0c321cb7
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-29 01:43:46 -0500

    Add correct guesses counter

--- src/components/FlagGuesser.jsx
+++ src/components/FlagGuesser.jsx
@@ -1,10 +1,11 @@
-import { Button, Image, Text, TextInput } from "@mantine/core";
+import { Button, Image, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
 
 const FlagGuesser = ({ countries }) => {
 const [currentFlag, setCurrentFlag] = useState(null);
 const [countriesList, setCountriesList] = useState([]);
 const [countriesCounter, setCountriesCounter] = useState(null);
+const [correctGuesses, setCorrectGuesses] = useState(0);
   
 useEffect(() => {
     const shuffledCountries = [...countries].sort(() => Math.random() - 0.5)
@@ -20,6 +21,7 @@ const checkGuess = (value) => {
             setCountriesList((prevList) => prevList.filter((country) => country !== currentFlag));
             setCountriesCounter((prevCounter) => prevCounter -1);
             setCurrentFlag(countriesList[1])
+            setCorrectGuesses((prev) => prev + 1)
         }
     } else {
         alert('Wrong')
@@ -36,8 +38,8 @@ const checkGuess = (value) => {
         <Button onClick={() => checkGuess("Random Wrong Country")}>Wrong</Button>
       </>
       }
-      
-      <Text>Countries & Territories Flags Counter: {countriesCounter}</Text>
+      <Text>Correct Guesses: {correctGuesses} / 250</Text>
+      <Text>Countries & territories left to guess: {countriesCounter}</Text>
       {countriesList.map((country, index) => (
         <p key={index}>{country.name}</p>
       ))}
