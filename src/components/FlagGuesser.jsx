6061449eab98f280c39f178218d0659020442367
Monica Alyssa <monica_alyssa@icloud.com>
2024-11-29 01:07:36 -0500

    Add logic to remove correct flag guess from list

--- src/components/FlagGuesser.jsx
+++ src/components/FlagGuesser.jsx
@@ -1,15 +1,25 @@
-import { Image, Text } from "@mantine/core";
+import { Button, Image, Text } from "@mantine/core";
 import { useEffect, useState } from "react";
 
 const FlagGuesser = ({ countries }) => {
 const [currentFlag, setCurrentFlag] = useState(null);
 const [countriesList, setCountriesList] = useState([]);
+const [countriesCounter, setCountriesCounter] = useState(null);
   
 useEffect(() => {
     const shuffledCountries = [...countries].sort(() => Math.random() - 0.5)
     setCountriesList(shuffledCountries)
     setCurrentFlag(shuffledCountries[0])
+    setCountriesCounter(shuffledCountries.length)
 }, [countries])
+
+const removeCorrectGuess = () => {
+    if (countriesList.includes(currentFlag)) {
+        setCountriesList((prevList) => prevList.filter((country) => country !== currentFlag));
+        setCountriesCounter((prevCounter) => prevCounter -1);
+        setCurrentFlag(countriesList[1])
+    }
+}
 
   return (
     <>
@@ -19,10 +29,11 @@ useEffect(() => {
         <Image w={200} src={currentFlag.flag}></Image>
       </>
       }
+      <Text>Countries & Territories Flags Counter: {countriesCounter}</Text>
+      
       {countriesList.map((country, index) => (
         <p key={index}>{country.name}</p>
       ))}
-
     </>
   );
 };
